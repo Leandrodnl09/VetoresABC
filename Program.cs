45cc0f88091e121a16d505b524b529486d752d95
Leandro Di Nardo Lazarin <leandrodnl09@gmail.com>
2022-08-04 20:21:14 -0300

    VetoresABC

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿// Exercício do curso Nélio Alves "Vetores A B C"
+// Esse programa será criado 3 vetores, o usuário ir digitar uma quantidade "N" ´de números nos Vetores A e B.
+// Em seguida o programa mostrará no vetor C a soma do vetor A e B;
+
+int N = int.Parse(Console.ReadLine());
+int [] A = new int[N];
+int [] B = new int[N];
+int [] C = new int[N];
+string [] X = Console.ReadLine().Split(' ');
+for (int i = 0; i < N; i++)
+{
+    A[i] = int.Parse(X[i]);
+}
+string [] Y = Console.ReadLine().Split(' ');
+for (int i = 0; i < N; i++)
+{
+    B[i] = int.Parse(Y[i]);
+}
+for (int i = 0; i < N; i++)
+{
+    C[i] = A[i] + B[i];
+    Console.Write(C[i] + " ");
+}
